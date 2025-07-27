4a73615994f28b82bb3d61b548b5f43b1290a1f8
Pohao <pohao121@gmail.com>
2025-07-27 14:34:11 +0800

    feat(model): 登入回傳資料型別設定

---
+++ src/app/models/user.ts
@@ -0,0 +1,9 @@
+export class User {
+  name!: string;
+  gender!: string;
+  birth_year!: number;
+  CDR_SUM!: number | null;
+  MMSE_Score!: number | null;
+  MEMORY!: number | null;
+  CDRGLOB!: number | null;
+}
