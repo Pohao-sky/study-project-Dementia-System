24f5c0a22b14a26d93120795019b75f55cea7532
Pohao <pohao121@gmail.com>
2025-08-14 16:57:07 +0800

    feat(TMTA&B): 第一個node會發光實作

--- src/app/trail-making-test-a-page/trail-making-test-a-page.component.spec.ts
+++ src/app/trail-making-test-a-page/trail-making-test-a-page.component.spec.ts
@@ -21,11 +21,11 @@ describe('TrailMakingTestAPageComponent', () => {
       expect(component).toBeTruthy();
     });
 
-    it('should determine connected nodes correctly', () => {
+  it('should determine connected nodes correctly', () => {
     const node1 = { label: 1, x: 0, y: 0 } as any;
+    expect((component as any).isNodeConnected(node1)).toBeTrue();
     const node2 = { label: 2, x: 0, y: 0 } as any;
     component.lines = [{ from: node1, to: node2 } as any];
-    expect((component as any).isNodeConnected(node1)).toBeTrue();
     expect((component as any).isNodeConnected(node2)).toBeTrue();
     const node3 = { label: 3, x: 0, y: 0 } as any;
     expect((component as any).isNodeConnected(node3)).toBeFalse();
